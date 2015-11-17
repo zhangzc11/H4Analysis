7c2ab1d0e7ff5ac1ea1d39de0765be1dd9ffa301
Simone Pigazzi <simone.pigazzini@live.it>
2015-11-17 12:27:44 +0100

    lxplus setup

---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/external/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
