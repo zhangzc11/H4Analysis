3122274d117cd8de9ca4711528770751b90845c0
Simone Pigazzi <simone.pigazzini@live.it>
2016-02-03 11:46:54 +0100

    fixing dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,5 @@
 
 source /afs/cern.ch/sw/lcg/external/gcc/4.9/x86_64-slc6-gcc49-opt/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.05.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+
+export LD_LIBRARY_PATH=DynamicTTree/lib/:$LD_LIBRARY_PATH
