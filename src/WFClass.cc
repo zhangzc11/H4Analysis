6cfb6d98b667b5105df3a630e1992f7c1bf04d89
michelif <francesco.micheli@cern.ch>
2016-07-06 15:53:50 +0200

    amplitude can be negative in life

--- src/WFClass.cc
+++ src/WFClass.cc
@@ -329,7 +329,7 @@ WFFitResults WFClass::TemplateFit(float offset, int lW, int hW)
         minimizer->SetTolerance(1e-3);
         minimizer->SetPrintLevel(0);
         minimizer->SetFunction(chi2);
-        minimizer->SetLimitedVariable(0, "amplitude", GetAmpMax(), 1e-2, 0., 4000.);
+        minimizer->SetLimitedVariable(0, "amplitude", GetAmpMax(), 1e-2, -4000., 4000.);
         minimizer->SetLimitedVariable(1, "deltaT", maxSample_*tUnit_, 1e-2, -1024.*tUnit_, 1024.*tUnit_);
         //---fit
         minimizer->Minimize();
