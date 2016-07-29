2067333072006b4575c5229230509c85831b7398
michelif <francesco.micheli@cern.ch>
2016-07-29 13:57:57 +0200

    adding back fibres on hodoscopes

--- src/PositionTree.cc
+++ src/PositionTree.cc
@@ -17,10 +17,12 @@ void PositionTree::Init()
     tree_->Branch("index", index, "index/l");
     X = new float[n_planes];
     Y = new float[n_planes];
+    nFibresOnX = new int[n_planes];
+    nFibresOnY = new int[n_planes];
     //---position branches
     tree_->Branch("n_planes", &n_planes, "n_planes/I");
-    tree_->Branch("n_hitsX", &n_hitsX, "n_hitsX/I");
-    tree_->Branch("n_hitsY", &n_hitsY, "n_hitsY/I");
     tree_->Branch("X", X, "X[n_planes]/F");
     tree_->Branch("Y", Y, "Y[n_planes]/F");
+    tree_->Branch("nFibresOnX", nFibresOnX, "nFibresOnX[n_planes]/I");
+    tree_->Branch("nFibresOnY", nFibresOnY, "nFibresOnY[n_planes]/I");
 }
